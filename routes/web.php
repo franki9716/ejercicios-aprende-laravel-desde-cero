a0974d95a7b17b76b5cd06c1b9d8643915ced399
Fran <camaralopezf@gmail.com>
2025-01-15 17:06:21 +0100

    ejercicio1

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,7 @@ Route::delete('/ejercicio1', function () {
 Route::patch('/ejercicio1', function () {
     return "PATCH OK";
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
