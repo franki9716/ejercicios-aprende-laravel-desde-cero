11a1b50120559b23dce9e3eb01c35b2cdb3419e6
Fran <camaralopezf@gmail.com>
2025-01-15 17:47:13 +0100

    ejercicio2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
@@ -28,4 +28,45 @@ Route::get('/ejercicio1', function () {
 
 Route::post('/ejercicio1', function () {
     return "POST OK";
+});
+
+
+Route::post('/ejercicio2/a', function(Request $req){
+    return $req;
+});
+
+
+Route::post('/ejercicio2/b', function(Request $req){
+    $price=$req->get('price');
+    $response=array();
+    if($price<0){
+        return Response::json([
+            'message' => "Price can't be less than 0"
+        ], 422); // Status code here
+    }else{
+        return $req;
+    }
+    
+});
+
+
+Route::post('/ejercicio2/c', function(Request $req){
+    $discount='';
+    $requestData = $req->all();
+    if(null!==$req->get('discount')){
+
+        $discount=$req->get('discount');
+        if($discount=='SAVE10'){
+            $requestData['price'] = ($requestData['price'] - ($requestData['price'] * 0.10));
+            $requestData['discount'] = 10;
+        }elseif($discount=='SAVE5'){
+            $requestData['price'] = ($requestData['price'] - ($requestData['price'] * 0.05));
+            $requestData['discount'] = 5;
+        }elseif($discount=='SAVE15'){
+            $requestData['price'] = ($requestData['price'] - ($requestData['price'] * 0.15));
+            $requestData['discount'] = 15;
+        }
+    }
+    $req->merge($requestData);
+    return $req;
 });
